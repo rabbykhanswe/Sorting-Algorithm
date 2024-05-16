23158b98f5047308c13a95787a497fec7d3ad31d
Rabby Khan <168404376+rabbykhanswe@users.noreply.github.com>
2024-05-16 12:23:45 +0600

    Update Insertion Sorting.cpp

--- Insertion Sorting.cpp
+++ Insertion Sorting.cpp
@@ -1,50 +1,34 @@
-// C++ program for insertion sort
-
 #include<stdio.h>
-
-// Function to sort an array using
-// insertion sort
-void insertionSort(int arr[], int n)
+void insertion_sorting(int arr[], int size)
 {
-    int i, key, j;
-    for (i = 1; i < n; i++) {
-        key = arr[i];
-        j = i - 1;
+int i,j;
 
-        // Move elements of arr[0..i-1],
-        // that are greater than key, 
-        // to one position ahead of their
-        // current position
-        while (j >= 0 && arr[j] > key) {
-            arr[j + 1] = arr[j];
-            j = j - 1;
-        }
-        arr[j + 1] = key;
-    }
+for(i=1; i<size; i++){
+int temp=arr[i];
+for(j=i-1; j>=0 && arr[j]>temp; j--){
+arr[j+1]=arr[j];
+}
+arr[j+1]=temp;
+
 }
 
-// A utility function to print an array
-// of size n
-void printArray(int arr[], int n)
-{
-    int i;
-    printf("Your Sorted Array is : ");
-    for (i = 0; i < n; i++)
-    {
-    	printf("%d ",arr[i]);
-	}
-	printf("\n");
 }
 
-// Driver code
+void print_array(int arr[], int size){
+int i;
+for(i=0; i<size; i++){
+printf("%d ",arr[i]);
+}
+}
 int main()
 {
-    int arr[] = { 12, 11, 13, 5, 6 };
-    int N = sizeof(arr) / sizeof(arr[0]);
+int arr[]={13,2,3,46,5,56,7,38,9,100};
+int size=sizeof(arr)/sizeof(arr[0]);
+printf("\nThe Unsorted Array is : ");
+print_array(arr,size);
+printf("\n\n\nThe Sorted Array is : ");
+insertion_sorting(arr,size);
+print_array(arr,size);
 
-    insertionSort(arr, N);
-    printArray(arr, N);
-
-    return 0;
-}
-
+return 0;
+} 
